070aad7a737de5d0abfd697dba30e43ae24d963b
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-30 17:31:02 -0300

    :card_file_box: added service to verify if username is not taken

---
+++ alura-pic/src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { SignupService } from './signup.service';
+import { debounceTime, switchMap, map, first } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService {
+  constructor(private signupService: SignupService) {}
+  checkUserNameTaken() {
+    return (control: AbstractControl) => {
+      return control.valueChanges
+        .pipe(debounceTime(300))
+        .pipe(
+          switchMap((userName) => {
+            return this.signupService.checkUserNameTaken(userName);
+          })
+        )
+        .pipe(map((isTaken) => (isTaken ? { userNameTaken: true } : null)))
+        .pipe(first()); // pega o primeiro valor emitido e completa o observable
+    };
+  }
+}

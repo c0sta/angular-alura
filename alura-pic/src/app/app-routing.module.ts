99cbedbb60ff32ee95a34932fdff00aae85594aa
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-30 16:57:45 -0300

    :art: added basic routes & child routes

--- alura-pic/src/app/app-routing.module.ts
+++ alura-pic/src/app/app-routing.module.ts
@@ -4,8 +4,29 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { SigninComponent } from './home/signin/signin.component';
+import { SignupComponent } from './home/signup/signup.component';
+import { AuthGuard } from './core/auth/auth.guard';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  // Mudar para SignupComponent dps
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: SigninComponent,
+      },
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+    ],
+  },
+
   {
     path: 'user/:username',
     component: PhotoListComponent,
@@ -18,7 +39,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}

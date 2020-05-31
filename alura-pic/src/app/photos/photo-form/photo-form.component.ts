39d1fda7ce73344739e2331d92d9f5bf01652a4d
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-31 16:57:28 -0300

    :sparkles: added photo form
    form that allow us to add photos to the application

--- alura-pic/src/app/photos/photo-form/photo-form.component.ts
+++ alura-pic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PhotoService } from '../photo/service/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.sass']
+  styleUrls: ['./photo-form.component.sass'],
 })
 export class PhotoFormComponent implements OnInit {
-
-  constructor() { }
+  photoForm: FormGroup;
+  file: File;
+  preview: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
-
+  upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe((resp) => {
+        console.log(resp);
+        this.router.navigate(['']);
+      });
+    // console.log(description, allowComments);
+    // console.log(this.file);
+  }
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => (this.preview = event.target.result);
+    reader.readAsDataURL(file);
+  }
 }
--- alura-pic/src/app/photos/photo-form/photo-form.module.ts
+++ alura-pic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { MessageModule } from 'src/app/shared/components/message/message.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MessageModule,
+    FormsModule,
+    RouterModule,
+    PhotoModule,
+    ImmediateClickModule,
+  ],
 })
 export class PhotoFormModule {}

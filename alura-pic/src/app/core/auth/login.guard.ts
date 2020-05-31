b3b9bfa0b009aafc207acf6d7cb81dcba839f820
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-31 16:59:01 -0300

    :art: changed old auth guard to this new login guard

---
+++ alura-pic/src/app/core/auth/login.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class LoginGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+  // se true, eu tenho acesso a rota. se false, não tenho acesso
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    console.log('Guardando rotas');
+    if (this.userService.isLogged()) {
+      this.router.navigate(['user/' + this.userService.getUserName()]);
+      return false;
+    }
+    return true;
+  }
+}
